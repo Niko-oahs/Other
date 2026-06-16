109da2d9095c38136771057cf167d7c6934a2063
Niko-oahs <oahshuang@gmail.com>
2026-06-16 15:53:30 +0800

    Add PreScript.sh to replace CentOS repository
    This script replaces the default CentOS repository with Aliyun mirrors and performs necessary updates.

---
+++ PreScript.sh
@@ -0,0 +1,40 @@
+# cenos更换源
+sudo cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+
+sudo bash -c 'cat << EOF > /etc/yum.repos.d/CentOS-Base.repo
+[centos]
+name=CentOS-\$releasever - Base - Aliyun
+baseurl=http://mirrors.aliyun.com/centos/\$releasever/os/\$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+enabled=1
+
+[centos-fasttrack]
+name=CentOS - Fasttrack
+mirrorlist=http://mirrors.aliyun.com/centos/7/fasttrack
+baseurl=http://mirrors.aliyun.com/centos/7/fasttrack
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+enabled=0
+
+[extras]
+name=CentOS-\$releasever - Extras
+baseurl=http://mirrors.aliyun.com/centos/\$releasever/extras/\$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+enabled=1
+
+[updates]
+name=CentOS-\$releasever - Updates
+baseurl=http://mirrors.aliyun.com/centos/\$releasever/updates/\$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+enabled=1
+EOF'
+
+sudo yum clean all
+sudo yum makecache
+
+# 必要更新
+sudo yum update -y
+sudo yum install -y yum-utils
