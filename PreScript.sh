f9fa5f59e65dcecc68cbb17288d41971ccd3a718
Niko-oahs <oahshuang@gmail.com>
2026-07-03 16:07:20 +0800

    Install and enable firewalld service
    Added commands to start and enable firewalld service.

--- PreScript.sh
+++ PreScript.sh
@@ -31,13 +31,15 @@ gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 enabled=1
 EOF'
-
 sudo yum clean all
 sudo yum makecache
 
 # 必要更新
 sudo yum update -y
 sudo yum install -y yum-utils
-
 # 安装防火墙：
 sudo yum install firewalld
+# 打开防火墙
+sudo systemctl start firewalld
+# 设置防火墙开机启动
+sudo systemctl enable firewalld
